c7490920d6d93f6a45b09d87635b7f88dcb871a7
Sunny7081 <aloks1769@gmail.com>
2026-06-12 03:06:51 +0530

    Update Java code

--- Day 4/p8.java
+++ Day 4/p8.java
@@ -1,2 +1,32 @@
 public class p8 {
+    public static void main(String [] args) {
+int n = 5;
+for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+ 
+// === INVERTED TRIANGLE ===
+for (int i = n; i >= 1; i--) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+ 
+// === NUMBER PYRAMID ===
+for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print(j + " ");
+    }
+    System.out.println();
+}
+    }
 }
---
+++ Day 4/p9.java
@@ -0,0 +1,14 @@
+public class p9
+{
+public static void main(String [] args)
+{
+public int[] arr;              
+arr = new int[5];      
+int[] arr2 = new int[5];
+int[] arr3 = {10, 20, 30, 40, 50};
+int[] arr4 = new int[]{10, 20, 30, 40, 50};
+System.out.println(arr3.length);   
+System.out.println(arr3[0]);       
+System.out.println(arr3[4]);       
+}
+}
